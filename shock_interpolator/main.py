3f5f37360c08927c213c613865cfa98496e27cad
alilasemi <alasemi@stanford.edu>
2021-11-18 17:59:18 -0800

    Add pressure gradient sensor on unstructured mesh

--- shock_interpolator/main.py
+++ shock_interpolator/main.py
@@ -1,10 +1,193 @@
-import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import rc
 from matplotlib.patches import Circle, Wedge, Polygon
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+import numpy as np
+import os
+import pickle
 import scipy
 from scipy.interpolate import lagrange
-import pickle
+
+
+def main():
+
+    # Number of interpolation points along the shock
+    n_points = 5
+    # Whether to process data from scratch or read from file
+    process_data = True
+    # Whether to plot one case, and if so, which case
+    just_one_case = True
+    if just_one_case:
+        plot_case = 'square'
+        plot_number = 40
+
+    # Load data
+    with open('data.pkl', 'rb') as outfile:
+        data = pickle.load(outfile)
+
+    # Geometries
+    geoms = {}
+    # TODO: Get naca geom
+    geoms['naca0012'] = points_on_diamond(np.array([0, 0]), 1, 12)
+    geoms['square'] = points_on_square(np.array([0, .5]), 1, 12)
+    geoms['diamond'] = points_on_diamond(np.array([0, 0]), 1, 12)
+    geoms['wedge'] = points_on_wedge(np.array([0, 0]), 1, 12)
+    geoms['triangle'] = points_on_triangle(np.array([1, 0]), 1, 12)
+    geoms['cylinder'] = points_on_cylinder(np.array([0, 0]), 1, 12)
+
+    if process_data:
+        # Loop over geometries
+        shock = {}
+        for case, results in data.items():
+            # TODO: Hack
+            if case != plot_case: continue
+            x_list, pressure_list, grad_list = results
+            shock[case] = []
+            print(f'Processing the {case} cases')
+            print('Working on case: ', end='', flush=True)
+            # Loop over cases
+            for i in range(len(x_list)):
+                # TODO: Hack
+                if i != plot_number:
+                    shock[case].append(None)
+                    continue
+                print(f'{i}, ', end='', flush=True)
+                x = x_list[i]
+                pressure = pressure_list[i]
+                grad = grad_list[i]
+                # If the case doesn't exist, skip it and append None
+                if x is None:
+                    shock[case].append(None)
+                    continue
+
+                # -- Find shock -- #
+                # Leftmost point, where the search begins
+                x_start = -5
+                # Search step size
+                dx = .01
+                # How far to go before quitting
+                stop_distance = 5
+                # Points in y-direction to start at
+                y_starts = np.linspace(-3, 3, n_points)
+
+                # Loop over all points in y
+                shock_x = np.empty((n_points, 2))
+                for y_idx, y_start in enumerate(y_starts):
+                    # Find the point closest to (x_start, y_start)
+                    start_index = np.argmin(np.linalg.norm(x - ([x_start, y_start]), axis=1))
+                    start_x = x[start_index]
+                    start_p = pressure[start_index]
+
+                    # Move from left to right, waiting for a pressure jump
+                    if np.abs(y_start) < .01:
+                        scaling = 1
+                    else:
+                        scaling = 1 / (np.abs(y_start)**2)
+                    jumps = 1 + np.array([.4, .5]) * scaling
+                    x_jump = []
+                    j = 0
+                    old_index = -1
+                    for i in range(int(stop_distance/dx)):
+                        index = np.argmin(np.linalg.norm(x - (start_x + [i * dx, 0]), axis=1))
+                        # No repeats
+                        if index == old_index: continue
+                        # If the pressure jumps, record it
+                        if pressure[index] > jumps[j]*start_p:
+                            # Reject large changes in y
+                            if np.abs(x[index, 1] - y_start) < .2:
+                                x_jump.append(x[index])
+                                j += 1
+                                if j == len(jumps): break
+                                old_index = index
+                    shock_x[y_idx, :] = np.mean(np.array(x_jump), axis=0)
+                # Save data
+                shock[case].append(shock_x)
+            print()
+
+            # Write to file
+            with open('shock.pkl', 'wb') as outfile:
+                pickle.dump(shock, outfile, protocol=pickle.HIGHEST_PROTOCOL)
+
+    else:
+        # Load data
+        with open('shock.pkl', 'rb') as outfile:
+            shock = pickle.load(outfile)
+
+    # Plot one case
+    if just_one_case:
+        shock_x = shock[plot_case][plot_number]
+        x = data[plot_case][0][plot_number]
+        pressure = data[plot_case][1][plot_number]
+        grad = data[plot_case][2][plot_number]
+        plot_data(plot_case, plot_number, n_points, shock_x, x, pressure, grad, geoms)
+    # Otherwise, plot them all and save to file
+    else:
+        for case, results in data.items():
+            x_list, pressure_list = results
+            print(f'Plotting the {case} cases')
+            print('Working on case: ', end='', flush=True)
+            # Loop over cases
+            for i in range(len(x_list)):
+                print(f'{i}, ', end='', flush=True)
+                shock_x = shock[case][i]
+                x = data[case][0][i]
+                pressure = data[case][1][i]
+                # If there's no data, skip this one
+                if x is None: continue
+                plot_data(case, i, n_points, shock_x, x, pressure, geoms,
+                        save_fig = True)
+            print()
+
+
+def plot_data(case, number, n_points, shock_x, x, pressure, grad, geoms,
+        save_fig = False):
+
+    # Construct interpolant for the shock
+    ref_nodes = np.polynomial.chebyshev.chebroots([0]*n_points + [1])
+    x_poly = lagrange(ref_nodes, shock_x[:, 0])
+    y_poly = lagrange(ref_nodes, shock_x[:, 1])
+    ref_plot_points = np.linspace(-1, 1, 100)
+
+    # Plot
+    rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+    rc('text', usetex=True)
+    fig = plt.figure(figsize=(7,7))
+    # Pressure gradient field
+    levels = np.linspace(0, np.max(grad[:, 0]), 8)
+    plt.tricontourf(x[:, 0], x[:, 1], grad[:, 0], cmap='cividis', levels =
+            levels)
+    # Solid body
+    x_geom = np.concatenate((geoms[case][:, 0], [geoms[case][0, 0]]))
+    y_geom = np.concatenate((geoms[case][:, 1], [geoms[case][0, 1]]))
+    plt.fill(x_geom, y_geom, color='white')
+    # Shock polynomial
+    plt.plot(x_poly(ref_plot_points), y_poly(ref_plot_points), '#ff7518', lw=3)
+    # Shock points
+    plt.plot(shock_x[:, 0], shock_x[:, 1], 'ko', ms=8)
+    # Grid points
+    #plt.plot(x[:, 0], x[:, 1], 'ko', ms=3)
+    # Axes
+    plt.xlabel('x (m)', fontsize=16)
+    plt.ylabel('y (m)', fontsize=16)
+    plt.xlim([-5, 5])
+    plt.ylim([-5, 5])
+    plt.tick_params(labelsize=12)
+    #plt.legend(loc='center left', fontsize = 20)
+    cbaxes = inset_axes(plt.gca(), width="3%", height="30%", loc=3)
+    cbar = plt.colorbar(cax=cbaxes, orientation = 'vertical')
+    cbar.set_label('$x$ Pressure Gradient', rotation=0, labelpad=0, y=1.15)
+
+    if save_fig:
+        os.makedirs(f'plots/{case}', exist_ok = True)
+        if number < 10:
+            file_name = f'plots/{case}/plot_0{number}'
+        else:
+            file_name = f'plots/{case}/plot_{number}'
+        plt.savefig(file_name, bbox_inches='tight')
+        plt.close()
+        plt.clf()
+    else:
+        plt.show()
 
 def points_on_line(x0, x1, n):
     '''
@@ -67,109 +250,5 @@ def points_on_cylinder(tip, d, n):
     y = np.sin(theta)
     return (d/2) * np.vstack([x, y]).T
 
-# Load data
-with open('data.pkl', 'rb') as outfile:
-    data = pickle.load(outfile)
-
-# Geometries
-geoms = {}
-geoms['naca0012'] = np.empty((10, 2))
-geoms['square'] = points_on_square(np.array([0, .5]), 1, 12)
-geoms['diamond'] = points_on_diamond(np.array([0, 0]), 1, 12)
-geoms['wedge'] = points_on_wedge(np.array([0, 0]), 1, 12)
-geoms['triangle'] = points_on_triangle(np.array([1, 0]), 1, 12)
-geoms['cylinder'] = points_on_cylinder(np.array([0, 0]), 1, 12)
-breakpoint()
-
-# Loop over geometries
-shock = {}
-for case, results in data.items():
-    x_list, pressure_list = results
-    shock[case] = []
-    print(f'Processing the {case} cases')
-    print('Working on case: ', end='', flush=True)
-    # Loop over cases
-    for i in range(len(x_list)):
-        # TODO: Hack
-        if i > 1: break
-        print(f'{i}, ', end='', flush=True)
-        x = x_list[i]
-        pressure = pressure_list[i]
-        # If the case doesn't exist, skip it and append None
-        if x is None:
-            shock[case].append(None)
-            continue
-
-        # -- Find shock -- #
-        # Leftmost point, where the search begins
-        x_start = -5
-        # Search step size
-        dx = .01
-        # How far to go before quitting
-        stop_distance = 5
-        # Points in y-direction to start at
-        n_points = 5
-        y_starts = np.linspace(-4, 4, n_points)
-
-        # Loop over all points in y
-        shock_x = np.empty((n_points, 2))
-        for y_idx, y_start in enumerate(y_starts):
-            # Find the point closest to (x_start, y_start)
-            start_index = np.argmin(np.linalg.norm(x - ([x_start, y_start]), axis=1))
-            start_x = x[start_index]
-            start_p = pressure[start_index]
-
-            # Move from left to right, waiting for a pressure jump
-            x_jump = np.empty((3, 2))
-            jumps = [1.2, 1.3, 1.4]
-            j = 0
-            for i in range(int(stop_distance/dx)):
-                index = np.argmin(np.linalg.norm(x - (start_x + [i * dx, 0]), axis=1))
-                # If the pressure jumps, record it
-                if pressure[index] > jumps[j]*start_p:
-                    x_jump[j] = x[index]
-                    j += 1
-                    if j == len(jumps): break
-            shock_x[y_idx, :] = np.mean(x_jump, axis=0)
-        # Save data
-        shock[case].append(shock_x)
-    print()
-
-# Pick case to plot
-case = 'wedge'
-number = 0
-shock_x = shock[case][number]
-x = data[case][0][number]
-pressure = data[case][1][number]
-
-# Construct interpolant for the shock
-ref_nodes = np.polynomial.chebyshev.chebroots([0]*n_points + [1])
-x_poly = lagrange(ref_nodes, shock_x[:, 0])
-y_poly = lagrange(ref_nodes, shock_x[:, 1])
-ref_plot_points = np.linspace(-1, 1, 100)
-
-# Plot
-rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
-rc('text', usetex=True)
-fig = plt.figure(figsize=(7,7))
-# Pressure field
-plt.tricontourf(x[:, 0], x[:, 1], pressure, cmap='cividis')
-# Solid body
-x_geom = np.concatenate((geoms[case][:, 0], [geoms[case][0, 0]]))
-y_geom = np.concatenate((geoms[case][:, 1], [geoms[case][0, 1]]))
-plt.fill(x_geom, y_geom, color='white')
-# Shock polynomial
-plt.plot(x_poly(ref_plot_points), y_poly(ref_plot_points), '#ff7518', lw=3)
-# Shock points
-plt.plot(shock_x[:, 0], shock_x[:, 1], 'ko', ms=8)
-# Grid points
-#plt.plot(x[:, 0], x[:, 1], 'ko', ms=3)
-# Axes
-plt.xlabel('x (m)', fontsize=16)
-plt.ylabel('y (m)', fontsize=16)
-plt.xlim([-5, 5])
-plt.ylim([-5, 5])
-plt.tick_params(labelsize=12)
-#plt.legend(loc='center left', fontsize = 20)
-plt.savefig('shock.pdf', bbox_inches='tight')
-plt.show()
+if __name__ == '__main__':
+    main()
--- shock_interpolator/read_data.py
+++ shock_interpolator/read_data.py
@@ -15,22 +15,30 @@ def main():
     for directory in os.listdir(cfd_results):
         # Name of case
         case = directory.split('_')[-1]
-        data[case] = ([], [])
+        #TODO: Hack
+        if case != 'square': continue
+        data[case] = ([], [], [])
+        print(f'Processing the {case} cases')
+        print('Working on case: ', end='', flush=True)
 
         # Loop over possible cases
         for i in range(n_cases):
+            print(f'{i}, ', end='', flush=True)
             # Data file
             file_name = f'{cfd_results}/{directory}/flow_{i}.vtu'
             # Check if it exists
             if os.path.exists(file_name):
                 # If it does, process it
-                x, pressure = process_data(file_name)
+                x, pressure, grad = process_data(file_name)
                 data[case][0].append(x)
                 data[case][1].append(pressure)
+                data[case][2].append(grad)
             # If it doesn't exist, append None
             else:
                 data[case][0].append(None)
                 data[case][1].append(None)
+                data[case][2].append(None)
+        print()
 
     # Write to file
     with open('data.pkl', 'wb') as outfile:
@@ -49,9 +57,42 @@ def process_data(file_name):
     # Extract pressure and location into Numpy arrays
     pressure = vtk_to_numpy(output.GetPointData().GetArray("Pressure"))
     x = vtk_to_numpy(output.GetPoints().GetData())
+    conn = vtk_to_numpy(output.GetCells().GetData())
+    # Remove the unnecessary number of vertices (there is a "4" every 4
+    # points...)
+    conn = conn[np.mod(np.arange(conn.size),5)!=0]
     # Remove the z-axis since it's 2D
     x = x[:, :2]
-    return x, pressure
+    n_pts = pressure.size
+    grad = np.empty((n_pts, 2))
+    # Loop over points
+    for i in range(n_pts):
+        # Cells containing this point
+        cells = np.argwhere(conn == i)[:, 0] // 4
+        # Loop cells
+        points_list = []
+        for cell in cells:
+            # Points in this cell
+            cell_points = conn[4*cell:4*cell+4]
+            points_list.append(cell_points)
+        # Get unique points in single array
+        points = np.unique(np.concatenate(points_list))
+        # Remove the original point
+        points = points[points != i]
+        # Compute distances
+        dist = x[points] - x[i]
+        dist_norm = np.linalg.norm(dist, axis=1)
+        dist_unit = dist / dist_norm.reshape(-1, 1)
+        # Compute pressure differences
+        diff = pressure[points] - pressure[i]
+        # Compute gradient in the direction of these points
+        grad_norm = diff / dist_norm
+        # Multiply with unit vector to get directional gradient
+        grad_vec = dist_unit * grad_norm.reshape(-1, 1)
+        # Sum all gradient vectors to get the resultant
+        grad[i] = np.sum(grad_vec, axis = 0)
+
+    return x, pressure, grad
 
 if __name__ == '__main__':
     main()
